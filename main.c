b695f611f045160842d7b2e5d4c3604fd6491668
a-ivie <sgcm9285@leeds.ac.uk>
2025-12-03 21:02:45 +0000

    making the menu look cute :)

--- main.c
+++ main.c
@@ -5,13 +5,14 @@
 #include <ctype.h>
 
 void print_menu(void) { 
-    printf("MENU: \n");
-    printf("1. List/ Add/ Edit Meals \n"
-           "2. Generate Weekly Plan \n"
-           "3. Show your statistics \n"
-           "4. Your staples and cost sharing analysis \n"
-           "5. Save your current plan to file \n"
-           "6. Exit and Save Data \n"); 
+    printf("---------------------MENU: -------------------\n");
+    printf("|1. List/ Add/ Edit Meals                    |\n"
+           "|2. Generate Weekly Plan                     |\n" 
+           "|3. Show your statistics                     |\n"
+           "|4. Your staples and cost sharing analysis   |\n"
+           "|5. Save your current plan to file           |\n"
+           "|6. Exit and Save Data                       |\n"
+           "---------------------------------------------- \n"); 
     }
 
     int main(void) { 
