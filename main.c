c04c45fe4afd05511c474fd61b6acf76f04a26df
a-ivie <sgcm9285@leeds.ac.uk>
2025-12-06 19:05:43 +0000

    defining meal structure

--- main.c
+++ main.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h> 
 #include <ctype.h>
+#include <math.h> 
+#include <time.h> 
 
-void print_menu(void) { 
-    printf("---------------------MENU: -------------------\n");
-    printf("|1. List/ Add/ Edit Meals                    |\n"
-           "|2. Generate Weekly Plan                     |\n" 
-           "|3. Show your statistics                     |\n"
-           "|4. Your staples and cost sharing analysis   |\n"
-           "|5. Save your current plan to file           |\n"
-           "|6. Exit and Save Data                       |\n"
-           "---------------------------------------------- \n"); 
-    }
+//print main menu 
+void print_menu(void) {
+    printf("\n--------------------MENU:------------------\n");
+    printf("| 1. List Available Meals                   |\n");
+    printf("| 2. Generate Weekly Plan                   |\n");
+    printf("| 3. Show your statistics                   |\n");
+    printf("| 4. Your staples and cost sharing analysis |\n");
+    printf("| 5. Save your current plan to file         |\n");
+    printf("| 6. Exit Program                           |\n");
+    printf("--------------------------------------------\n");
+}
 
-    int main(void) { 
+int main(void) {
     print_menu(); 
     return 0; 
 }
+
+//define constants 
+#define MAX_MEALS 50
+#define MAX_NAME_LEN 128
+#define MAX_DAYS 7
+#define MAX_MEALS_PER_DAY 3
+#define MAX_STAPLES 10
+#define MAX_MEAL_USAGE_LEN 512
+
+// meal structure 
+typedef struct { 
+    char mealName[MAX_NAME_LEN]; 
+    float CostPerServing; 
+    int TotalCalories; 
+    int ProteinIntakeGrams; 
+    int FoodCategory; 
+    int isVegetarian; 
+} Meal; 
+
+// daily plan 
